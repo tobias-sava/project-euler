bf01e63fd12135bb60fe5c76c58f209860a1c45a
tobias-sava <tobias.svgb@gmail.com>
2025-05-09 15:59:32 +0300

    in progress

--- P7.py
+++ P7.py
@@ -12,3 +12,43 @@
 # - If prime increment count and n
 # - If not prime increment n
 
+import math
+
+
+def func(maxprime):
+
+    count = 0 # keeping track of primes found
+
+    n = 0 # number being checked if prime until desired count is reached
+
+    d = 2 # divisor
+
+    while count != maxprime:
+        
+        # base cases
+        if n < 2:
+            n += 1
+        
+        if n == 2:
+            count += 1
+            n += 1
+        
+        if n % 2 == 0:
+            n += 1
+        
+            # trial divison
+            while d <= math.sqrt(n):
+                
+                if n % d == 0:
+                    n += 1
+                    break
+                else:
+                    d += 1
+                
+            count += 1
+            n += 1
+
+        return n
+    
+print(func(10001))
+        
